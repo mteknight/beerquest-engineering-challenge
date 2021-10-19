b4d798937ab5c19be04453cbaba77bb39240c365
Alexandre Fernandes <mteknight@gmail.com>
2021-10-19 18:12:52 +0100

    added missing test class

---
+++ Beer Quest/BeerQuest.API.Tests/PubControllerTests.cs
@@ -0,0 +1,44 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+using AutoFixture.Xunit2;
+
+using BeerQuest.API.Controllers;
+using BeerQuest.Domain;
+using BeerQuest.Domain.Services;
+
+using FluentAssertions;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Moq;
+
+using Xunit;
+
+namespace BeerQuest.API.Tests
+{
+    public class PubControllerTests
+    {
+        [Theory]
+        [AutoData]
+        public async Task GivenPubExists_WhenQueryingPubsByName_ThenReturnPub(Pub pub)
+        {
+            // Arrange
+            var mockedPubService = new Mock<IPubService>();
+            mockedPubService
+                .Setup(service => service.Get(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(pub);
+
+            var controller = new PubController(mockedPubService.Object);
+
+            // Act
+            var result = await controller.Get(pub.Name);
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            result.As<OkObjectResult>().Value.As<Pub>().Name.Should().NotBeNull("The pub instance should be returned in the response.");
+        }
+
+
+    }
+}
