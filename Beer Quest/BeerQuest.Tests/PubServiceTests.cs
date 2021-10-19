56a40e994233eaffb00964ef1521e5f2fb8355d2
Alexandre Fernandes <mteknight@gmail.com>
2021-10-19 13:17:11 +0100

    added test for httpclient service

--- Beer Quest/BeerQuest.Tests/PubServiceTests.cs
+++ Beer Quest/BeerQuest.Tests/PubServiceTests.cs
@@ -1,8 +1,15 @@
 using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 using AutoFixture.Xunit2;
 
+using Dawn;
+
 using FluentAssertions;
+
+using Newtonsoft.Json;
 
 using Xunit;
 
@@ -12,24 +19,69 @@ namespace BeerQuest.Tests
     {
         [Theory]
         [AutoData]
-        public void GivenPubExists_WhenGettingPubByName_ExpectPubRetrieved(string name)
+        public async Task GivenPubExists_WhenGettingPubByName_ExpectPubRetrieved(string name)
         {
             // Arrange
             var service = new PubService();
 
             // Act
-            var pub = service.Get(name);
+            var pub = await service.Get(name);
 
             // Assert
             pub.Should().NotBeNull($"A pub named '{name}' exists and is part of the Beer Quest.");
+        }
+
+        [Theory]
+        [AutoData]
+        public void GivenRequestUnsuccessful_WhenMakingHttpGetRequest_ExpectHttpRequestException(string uri)
+        {
+            // Arrange
+            var apiClient = new HttpClientService();
+
+            // Act
+            async Task SutCall()
+            {
+                await apiClient.Get<Pub>(uri);
+            }
+
+            Action sutCall = async () => await SutCall();
+
+            // Assert
+            sutCall.Should().ThrowExactly<HttpRequestException>("Http call must ensure a successful result.");
+        }
+    }
+
+    public class HttpClientService
+    {
+        public Task<T> Get<T>(string uri)
+        {
+            throw new NotImplementedException();
         }
     }
 
     public class PubService
     {
-        public Pub Get(string name)
+        private const string api = "https://datamillnorth.org/api/table/wk7xz_hf8bv";
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public PubService(IHttpClientFactory httpClientFactory)
         {
-            throw new NotImplementedException();
+            this.httpClientFactory = Guard.Argument(httpClientFactory, nameof(httpClientFactory)).NotNull().Value;
+        }
+
+        public async Task<Pub> Get(
+            string name,
+            CancellationToken cancellationToken = default)
+        {
+            var uri = $"{api}?name={name}";
+            using var client = this.httpClientFactory.CreateClient();
+            var response = await client.GetAsync(uri, cancellationToken);
+
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync(cancellationToken);
+
+            return JsonConvert.DeserializeObject<Pub>(json);
         }
     }
 
