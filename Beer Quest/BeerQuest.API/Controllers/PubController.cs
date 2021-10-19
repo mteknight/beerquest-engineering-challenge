3f862bc0781145d8cc40c0c27a91edf97b1f71c8
Alexandre Fernandes <mteknight@gmail.com>
2021-10-19 18:06:06 +0100

    added success tests for pub controller

---
+++ Beer Quest/BeerQuest.API/Controllers/PubController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeerQuest.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PubController : ControllerBase
+    {
+        public async Task<IActionResult> Get([FromQuery] string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
